6c497fa7b80b9cd33f475411d8e0a49af7e1b600
machaoui <machaoui@leyton.com>
2023-03-19 19:44:36 +0000

    leyron/ma: news pages

--- utils/custom_web_loader.ts
+++ utils/custom_web_loader.ts
@@ -25,10 +25,10 @@ export class CustomWebLoader
 
   async load(): Promise<Document[]> {
     const $ = await this.scrape();
-    const title = $('h1.entry-title').text();
-    const date = $('meta[property="article:published_time"]').attr('content');
+    const title = $('h1.ori-inshgit_header-title').text();
+    const date = $('ul.ori-inshgit_meta-details').attr('content');
 
-    const content = $('.entry-content')
+    const content = $('.ori-insight_content')
       .clone()
       .find('div.elementor, style')
       .remove()
